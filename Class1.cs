66ea066d4e2e5b78ab7967929302584797171675
Ramon Quiambao <rquiambao@dsai.ca>
2020-12-31 15:57:45 -0500

    Code clean up and reorganization

--- Class1.cs
+++ Class1.cs
@@ -5,12 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 //Load Autodesk
-using Autodesk.Revit.ApplicationServices;
+//using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-//Not sure if UI Selection needed
-using Autodesk.Revit.UI.Selection;
+//using Autodesk.Revit.UI.Selection;
 
 namespace DrawFRRLines
 {
@@ -24,8 +23,8 @@ namespace DrawFRRLines
             UIApplication uiapp = commandData.Application;
             Document doc = uiapp.ActiveUIDocument.Document;
 
-            //Get active view (THIS CAN BE REMOVED BECAUSE OF doc.ActiveView.Id in the FilteredElementCollector below)
-            Autodesk.Revit.DB.View activeView = doc.ActiveView;
+            //Get active view
+            View activeView = doc.ActiveView;
 
             //Get elements in active view
             FilteredElementCollector activeViewElements = new FilteredElementCollector(doc, doc.ActiveView.Id);
@@ -33,81 +32,89 @@ namespace DrawFRRLines
             //Filter wall elements from active view elements
             ICollection<Element> wallsView = activeViewElements.OfCategory(BuiltInCategory.OST_Walls).ToElements();
 
+            //Wall storage lists
+            IList<Wall> wallNone = new List<Wall>();
+            IList<Wall> wall00 = new List<Wall>();
+            IList<Wall> wall45 = new List<Wall>();
+            IList<Wall> wall60 = new List<Wall>();
+            IList<Wall> wall90 = new List<Wall>();
+            IList<Wall> wall120 = new List<Wall>();
+            IList<Wall> wall180 = new List<Wall>();
+
+            //Get parameter
+            foreach (Element wallElem in wallsView)
+            {
+                Wall wallInst = wallElem as Wall;
+                Parameter fireRatingParam = wallInst.WallType.LookupParameter("Fire Rating");
+                String paramValue = fireRatingParam.AsString();
+                //Sorting walls in active view
+                if (paramValue == "00")
+                {
+                    wall00.Add(wallInst);
+                }
+                if (paramValue == "45")
+                {
+                    wall45.Add(wallInst);
+                }
+                if (paramValue == "60")
+                {
+                    wall60.Add(wallInst);
+                }
+                if (paramValue == "90")
+                {
+                    wall90.Add(wallInst);
+                }
+                if (paramValue == "120")
+                {
+                    wall120.Add(wallInst);
+                }
+                if (paramValue == "180")
+                {
+                    wall180.Add(wallInst);
+                }
+                else
+                {
+                    wallNone.Add(wallInst);
+                }
+            }
+
             //Get line styles in project
             Category linesCat = doc.Settings.Categories.get_Item(BuiltInCategory.OST_Lines);
+
+            ///Getting the Category and GraphicsStyle can probably be turned into a method
+
+            //FRR NON-RATED
+            Category l000 = linesCat.SubCategories.get_Item("FIRE-000 Non-Rated Fire Separation");
+            GraphicsStyle gs000 = l000.GetGraphicsStyle(GraphicsStyleType.Projection);
+
+            //FRR 45 MIN
+            Category l045 = linesCat.SubCategories.get_Item("FIRE-045 0.75 Hour");
+            GraphicsStyle gs045 = l045.GetGraphicsStyle(GraphicsStyleType.Projection);
+
+            //FRR 1H
             Category l060 = linesCat.SubCategories.get_Item("FIRE-060 1 Hour");
             GraphicsStyle gs060 = l060.GetGraphicsStyle(GraphicsStyleType.Projection);
 
-            //Location sotrage list
-            IList<Location> loctest = new List<Location>();
+            //FRR 1.5H
+            Category l090 = linesCat.SubCategories.get_Item("FIRE-090 1.5 Hour");
+            GraphicsStyle gs090 = l090.GetGraphicsStyle(GraphicsStyleType.Projection);
 
-            //Location curve storage list
-            IList<LocationCurve> locCurve = new List<LocationCurve>();
+            //FRR 2H
+            Category l120 = linesCat.SubCategories.get_Item("FIRE-120 2 Hour");
+            GraphicsStyle gs120 = l120.GetGraphicsStyle(GraphicsStyleType.Projection);
 
-            //Line storage list
-            IList<Line> wallLine = new List<Line>();
-
-            //Curve storage list
-            IList<Curve> wallCurve = new List<Curve>();
+            //FRR 3H
+            Category l180 = linesCat.SubCategories.get_Item("FIRE-180 3 Hour");
+            GraphicsStyle gs180 = l180.GetGraphicsStyle(GraphicsStyleType.Projection);
 
             //Get location curves
             foreach (Element wallElem in wallsView)
             {
                 Wall wallInst = wallElem as Wall;
-                //Autodesk.Revit.DB.LocationCurve position = wallElem.Location;
-                //loctest.Add(position);
                 LocationCurve lcurve = wallInst.Location as LocationCurve;
                 Curve curve = lcurve.Curve;
                 wallCurve.Add(curve);
             }
-
-            ////Wall storage lists
-            //IList<Wall> wallNone = new List<Wall>();
-            //IList<Wall> wall00 = new List<Wall>();
-            //IList<Wall> wall45 = new List<Wall>();
-            //IList<Wall> wall60 = new List<Wall>();
-            //IList<Wall> wall90 = new List<Wall>();
-            //IList<Wall> wall120 = new List<Wall>();
-            //IList<Wall> wall180 = new List<Wall>();
-
-            ////Get parameter
-            //foreach (Element wallElem in wallsView)
-            //{
-            //    Wall wallInst = wallElem as Wall;
-            //    Parameter fireRatingParam = wallInst.WallType.LookupParameter("Fire Rating");
-            //    //Task dialog is for confirming that the parameter value is being extracted
-            //    //TaskDialog.Show("FRR: ", fireRatingParam.AsString());
-            //    String paramValue = fireRatingParam.AsString();
-            //    //Sorting walls in active view
-            //    if (paramValue == "00")
-            //    {
-            //        wall00.Add(wallInst);
-            //    }
-            //    if (paramValue == "45")
-            //    {
-            //        wall45.Add(wallInst);
-            //    }
-            //    if (paramValue == "60")
-            //    {
-            //        wall60.Add(wallInst);
-            //    }
-            //    if (paramValue == "90")
-            //    {
-            //        wall90.Add(wallInst);
-            //    }
-            //    if (paramValue == "120")
-            //    {
-            //        wall120.Add(wallInst);
-            //    }
-            //    if (paramValue == "180")
-            //    {
-            //        wall180.Add(wallInst);
-            //    }
-            //    else
-            //    {
-            //        wallNone.Add(wallInst);
-            //    }
-            //}
 
             //Transaction Reference
             Transaction trans = new Transaction(doc);
@@ -116,9 +123,6 @@ namespace DrawFRRLines
             {
                 //Create new line
                 DetailCurve frrLine = doc.Create.NewDetailCurve(activeView, i);
-
-                //Get Lline style parameter
-                //Parameter lineStyle = frrLine.LookupParameter("Line Style");
 
                 //Set line style
                 frrLine.LineStyle = gs060;
